7662c5336df509744c04db6a9f67beba89413118
Shilpita Biswas <shilpita@Shilpitas-MacBook-Air.local>
2020-11-19 15:55:25 -0800

    Fix test issues

--- tests/tests.py
+++ tests/tests.py
@@ -1,14 +1,15 @@
 from click.testing import CliRunner
-from ..autoflow.main import new, hello
+from autoflow.main import new, hello
 
 def test_new():
     runner = CliRunner()
-    result = runner.invoke('new')
+    result = runner.invoke(new)
     assert result.exit_code == 0
-    assert result.output == '🔥 creates new project'
+    assert type(result.output) == str
+    assert result.output.rstrip("\n") == '🔥 creates new project'
 
 def test_hello():
     runner = CliRunner()
-    result = runner.invoke('hello')
+    result = runner.invoke(hello)
     assert result.exit_code == 0
-    assert result.output == 'Hello'
+    assert result.output.rstrip("\n") == 'Hello'
