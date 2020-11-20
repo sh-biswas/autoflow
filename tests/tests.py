6e0bcc3da0d0257e83ec241001eb85de9da5355b
Shilpita Biswas <shilpita@Shilpitas-MacBook-Air.local>
2020-11-19 16:12:36 -0800

    Fix linting issues

--- tests/tests.py
+++ tests/tests.py
@@ -1,6 +1,7 @@
 from click.testing import CliRunner
 from autoflow.main import new, hello
 
+
 def test_new():
     runner = CliRunner()
     result = runner.invoke(new)
@@ -8,8 +9,10 @@ def test_new():
     assert type(result.output) == str
     assert result.output.rstrip("\n") == '🔥 creates new project'
 
+
 def test_hello():
     runner = CliRunner()
     result = runner.invoke(hello)
     assert result.exit_code == 0
     assert result.output.rstrip("\n") == 'Hello'
+    
